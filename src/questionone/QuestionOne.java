1bf841076c4026493552a786044e5222d44848d5
Amey <ameykurade60@gmail.com>
2023-08-04 21:47:47 +0530

    Init

---
+++ src/questionone/QuestionOne.java
@@ -0,0 +1,15 @@
+package questionone;
+import java.util.*;
+
+public class QuestionOne{
+    public static void main(String[] args) {
+        int a = 300, b = 0, c = 0;
+        if (a >= 400) {
+            b = 300;
+            a = 200;
+            System.out.println(b + "" + c);
+        }
+    }
+}
+
+//Error because c is not assigned
