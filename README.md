328172b4201eef085cc2d395a2626b5b649002ec
Amey <ameykurade60@gmail.com>
2023-08-04 22:03:52 +0530

    Init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Question-4-1
+Write code and show output
+
+# Answer
+0200
---
+++ src/question1/Question1.java
@@ -0,0 +1,12 @@
+package question1;
+import java.util.*;
+
+public class Question1{
+    public static void main(String[] args) {
+        int a=300, b=0, c;
+        if (a >= 400)
+            b=300;
+            c=200;
+        System.out.println(b + "" + c);
+    }
+}
